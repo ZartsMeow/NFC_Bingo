2489fe8d44333530e94072487e953d11ca9bcad2
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2024-11-21 21:29:39 -0800

    seperate out the prompts into another file

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-const promptList = [
-    "Shibou's metal music",
-    "Lurker appears",
-    "TastyPi maths",
-    "Edgy Jocose",
-    "Meowz friend gc",
-    "Cute Usaken",
-    "Cannibalism mention",
-    "Jerry with DnD",
-    "Phobi appears & leaves",
-    "Vovgus & Phobi flirting",
-    "\"I miss my wife\"",
-    "Shrimp insults Cris",
-    "Actually serious",
-    "Narwhal karaoke",
-    "Shad dropping wisdom",
-    "Jocose being a cute bf",
-    "Random Terra gif",
-    "Move to #silliness",
-    "Ddouble being old",
-    "Actually wholesome",
-    "Multi convo chat",
-    "No romance Meowz",
-    "Flustered Cris",
-    "Bullied Cris"
-];
-const freeList = [
-    "Therapy",
-    "Jocussy"
-];
 var board = [];
 var boardActive = Array(25).fill(false);
 var boardDisp = [];
---
+++ prompts.js
@@ -0,0 +1,30 @@
+const promptList = [
+    "Shibou's metal music",
+    "Lurker appears",
+    "TastyPi maths",
+    "Edgy Jocose",
+    "Meowz friend gc",
+    "Cute Usaken",
+    "Cannibalism mention",
+    "Jerry with DnD",
+    "Phobi appears & leaves",
+    "Vovgus & Phobi flirting",
+    "\"I miss my wife\"",
+    "Shrimp insults Cris",
+    "Actually serious",
+    "Narwhal karaoke",
+    "Shad dropping wisdom",
+    "Jocose being a cute bf",
+    "Random Terra gif",
+    "Move to #silliness",
+    "Ddouble being old",
+    "Actually wholesome",
+    "Multi convo chat",
+    "No romance Meowz",
+    "Flustered Cris",
+    "Bullied Cris"
+];
+const freeList = [
+    "Therapy",
+    "Jocussy"
+];
